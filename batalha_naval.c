2c58070e3feed8bbcf1d9838e38335942e4bb21f
Jonatan da Silva Schmidt <jonatank9@hotmail.com>
2025-11-18 19:35:53 +0000

    renovado

--- batalha_naval.c
+++ batalha_naval.c
@@ -2,59 +2,62 @@
 
 #define LINHAS 15
 #define COLUNAS 15
+#define NAVIOS 4
+#define TAMANHO_NAVIO 3
+#define TENTATIVAS 25
+
+// Função para inicializar o tabuleiro (vai se repetir mais tarde várias vezes, chamando a cada tentativa do jogador)
+
+void inicializarTabuleiro(int tabuleiro[LINHAS][COLUNAS]) {
+    int x[LINHAS];
+    char y[COLUNAS];
+
+    for (int i = 0; i < LINHAS; i++) {
+        x[i] = i + 1; // Preenche os números para as linhas
+        if (i == 0) {
+            printf(" "); // Espaço inicial para o cabeçalho
+        }
+        printf(" %2d", x[i]); // Imprime o cabeçalho das linhas
+    }
+    printf("\n");
+    for (int i = 0; i < LINHAS; i++) {
+        if (i < COLUNAS) {
+            y[i] = 'A' + i; // Preenche as letras para as colunas
+            printf("%c ", y[i]); // Imprime o cabeçalho das colunas
+    }
+        for (int j = 0; j < COLUNAS; j++) {
+            tabuleiro[i][j] = '~'; // Inicializa o tabuleiro com água
+            printf(" %c ", tabuleiro[i][j]); // Imprime o tabuleiro inicializado
+        }
+    printf("\n");
+    }
+     printf("\nlegenda: ~ = água, # = navio, X = acerto, O = erro\n\n");
+}
 
 int main() {
 
-    // Criação do cabeçalho da matriz 10x10 para o tabuleiro
+    // Criação do cabeçalho da matriz
     
     int tabuleiro[LINHAS][COLUNAS];
     char x[LINHAS];
     int y[COLUNAS];
+    int tentativas = TENTATIVAS;
+    int navios = NAVIOS;
+    int tamanho_navio = TAMANHO_NAVIO;
 
-    // Inicialização do tabuleiro com água (0), usando loop aninhados para gerar toda a matriz
-    for (int i = 0; i < LINHAS; i++) {
-        for (int j = 0; j < COLUNAS; j++) {
-            tabuleiro[i][j] = 0; // Inicializa todas as posições com 0, já que 0 representa água
-        }
-    }
-
-        // Posicionamento de dois navios com valor 3 na matriz
-    for (int i = 3; i < 6; i++) {
-        tabuleiro[2][i] = 3; // Navio horizontal na linha 3, colunas D a F
-    }
-
-    for (int i = 5; i < 8; i++) {
-        tabuleiro[i][7] = 3; // Navio vertical na coluna H, linhas 6 a 8
-    }
-
-    // Impressão do tabuleiro formatado
+    // Impressão do tabuleiro em branco
 
     printf("*** Tabuleiro de Batalha Naval ***\n\n");
     printf("**********Desafio Novato**********\n\n");
 
-    // Impressão do cabeçalho com letras
- 
-    for (int i = 0; i < LINHAS; i++) {
-        x[i] = 'A' + i; // Preenche o array x com letras
-        if (i == 0) {
-            printf("   "); // Espaço inicial para alinhar o cabeçalho
-        }
-        printf(" %c ", x[i]); // Imprime as letras do cabeçalho
-    }
-   
+    printf("Você terá ao todo %d tentativas para afundar os %d navios inimigos, cada um com tamanho %d espaços.\n\n", tentativas, navios, tamanho_navio);
+    printf("Boa sorte, comandante!\n\n");
 
-    // Impressão das linhas do tabuleiro com números e valores
-    for (int i = 0; i < COLUNAS; i++) {
-            y[i] = i + 1; // Preenche o array y com números
-            if (i < 9 ) {
-            printf("\n %d ", y[i]);
-            } else {
-            printf("\n%d ", y[i]);
-            }
-        for (int j = 0; j < LINHAS; j++) {
-            printf(" %d ", tabuleiro[i][j]);
-        }
-    }
+
+    inicializarTabuleiro(tabuleiro);
+
+
+    
 
 
 
