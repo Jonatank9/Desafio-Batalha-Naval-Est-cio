70b64f2e38c416e3d3376dbdc19a11d2040d1120
Jonatan da Silva Schmidt <jonatank9@hotmail.com>
2025-11-18 13:06:45 +0000

    FASE 2 - MATRIZ OK

--- batalha_naval.c
+++ batalha_naval.c
@@ -1,16 +1,19 @@
 #include <stdio.h>
 
+#define LINHAS 15
+#define COLUNAS 15
+
 int main() {
 
-    // Criação da matriz 10x10 para o tabuleiro
+    // Criação do cabeçalho da matriz 10x10 para o tabuleiro
     
-    char x[10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
-    int y[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    int tabuleiro[10][10];
+    int tabuleiro[LINHAS][COLUNAS];
+    char x[LINHAS];
+    int y[COLUNAS];
 
     // Inicialização do tabuleiro com água (0), usando loop aninhados para gerar toda a matriz
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
+    for (int i = 0; i < LINHAS; i++) {
+        for (int j = 0; j < COLUNAS; j++) {
             tabuleiro[i][j] = 0; // Inicializa todas as posições com 0, já que 0 representa água
         }
     }
@@ -30,22 +33,25 @@ int main() {
     printf("**********Desafio Novato**********\n\n");
 
     // Impressão do cabeçalho com letras
-    for (int i = 0; i < 10; i++) {
-            if (i == 0) {
-            printf("    %c ", x[i]);
-            } else {
-            printf(" %c ", x[i]);
+ 
+    for (int i = 0; i < LINHAS; i++) {
+        x[i] = 'A' + i; // Preenche o array x com letras
+        if (i == 0) {
+            printf("   "); // Espaço inicial para alinhar o cabeçalho
+        }
+        printf(" %c ", x[i]); // Imprime as letras do cabeçalho
     }
-    }
+   
 
     // Impressão das linhas do tabuleiro com números e valores
-    for (int i = 0; i < 10; i++) {
-            if (i < 9) {
+    for (int i = 0; i < COLUNAS; i++) {
+            y[i] = i + 1; // Preenche o array y com números
+            if (i < 9 ) {
             printf("\n %d ", y[i]);
             } else {
             printf("\n%d ", y[i]);
             }
-        for (int j = 0; j < 10; j++) {
+        for (int j = 0; j < LINHAS; j++) {
             printf(" %d ", tabuleiro[i][j]);
         }
     }
